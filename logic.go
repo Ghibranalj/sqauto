5aac851e04f53d0bb70bac12075325998b673c14
Ghibranalj <24712554+Ghibranalj@users.noreply.github.com>
2024-01-15 18:51:49 +0100

    fix: infinite recursion

--- logic.go
+++ logic.go
@@ -24,7 +24,7 @@ func includedIface(t, iface reflect.Type) bool {
 	case reflect.Struct:
 		return implements(t, iface)
 	case reflect.Ptr, reflect.Slice, reflect.Array, reflect.Map:
-		return includedIface(t, iface)
+		return includedIface(t.Elem(), iface)
 	case reflect.Bool, reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
 		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
 		reflect.Float32, reflect.Float64, reflect.String, reflect.Complex64, reflect.Complex128:
